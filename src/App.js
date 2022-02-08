d916da4c92bf0490222e1029d9f15140b1f98f98
Joe Peterson <joe@code.wildlava.com>
2022-02-08 15:18:31 -0700

    Replace demo content with a formation loaded from Contentful

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const space_id = process.env.REACT_APP_SPACE_ID;
+const access_token = process.env.REACT_APP_DELIVERY_TOKEN;
+
+const query = `
+{
+  formationCollection {
+    items {
+      name
+      diagram {
+        url
+      }
+    }
+  }
+}
+`;
+
 function App() {
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/${space_id}/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        setPage(data.formationCollection.items[0]);
+      });
+  }, []);
+
+  if (!page) {
+    return "Loading...";
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={page.diagram.url} className="App-diagram" alt="diagram" />
+        <p>{page.name}</p>
       </header>
     </div>
   );
