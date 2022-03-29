9bb93e44e8944944b360117ae8fae60759fa8ed2
Joe Peterson <joe@code.wildlava.com>
2022-03-29 15:26:30 -0600

    Enable queries for sets of formations having a given size

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import './App.css';
 const space_id = process.env.REACT_APP_SPACE_ID;
 const access_token = process.env.REACT_APP_DELIVERY_TOKEN;
 
-const query = `
-{
-  formationCollection {
-    items {
-      name
-      diagram {
-        url
-      }
-    }
-  }
-}
-`;
-
 function App() {
-  const [formation, setFormation] = useState(null);
+  const [formationSize, setFormationSize] = useState(8);
+  const [formations, setFormations] = useState(null);
+  const [curFormation, setCurFormation] = useState(0);
 
   useEffect(() => {
+    const query = `
+    {
+      formationCollection(where: { size: ${formationSize} }, order: [code_ASC]) {
+        items {
+          name
+          code
+          diagram {
+            url
+          }
+        }
+      }
+    }
+    `;
+
     window
       .fetch(`https://graphql.contentful.com/content/v1/spaces/${space_id}/`, {
         method: "POST",
@@ -36,20 +39,29 @@ function App() {
           console.error(errors);
         }
 
-        setFormation(data.formationCollection.items[0]);
+        setCurFormation(0); /* <-- This setter needs to be run first for now,
+                                   but try running it second when React 18 arrives. */
+        setFormations(data.formationCollection.items);
       });
-  }, []);
+  }, [formationSize]);
 
-  if (!formation) {
-    return "Loading...";
+  if (!formations) {
+    return "Loading from Contentful...";
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={formation.diagram.url} className="App-diagram" alt="diagram" />
-        <p>{formation.name}</p>
-      </header>
+      <select defaultValue="8" onChange={e => setFormationSize(parseInt(e.target.value, 10))}>
+        {[...Array(19).keys()].map((value, index) => (
+          <option key={index} value={index+2}>{index+2}-way</option>
+        ))}
+      </select>
+      <p>Number of formations: {formations.length}</p>
+      <img src={formations[curFormation].diagram.url} className="App-diagram" alt="diagram" />
+      <h2><p>{formations[curFormation].name}</p></h2>
+      <p>{formations[curFormation].code}</p>
+      <button disabled={curFormation <= 0} onClick={() => setCurFormation(curFormation - 1)}>-</button>
+      <button disabled={curFormation >= (formations.length - 1)} onClick={() => setCurFormation(curFormation + 1)}>+</button>
     </div>
   );
 }
